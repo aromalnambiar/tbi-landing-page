62494acef521bc05b2d6a67271af2c0699528ca9
Aromal Nambiar <aromalndr@gmail.com>
2023-02-07 19:01:03 +0530

    today update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+drawer = document.querySelector(".drawer")
+drawer.onclick = function () {
+    navBar = document.querySelector(".right_head")
+    navBar.classList.toggle("active")
+}
+
+
+// route = document.querySelector(".route")
+// route   .onclick = function () {
+//     navBar = document.querySelector(".right_head")
+//     navBar.classList.toggle("active")
+// }
